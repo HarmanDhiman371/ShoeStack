36fe7c3473ecd10fb40f78c4f1aa99e4caebc683
jaichopra0702 <jaichopra0702@gmail.com>
2025-02-28 01:35:27 +0530

    Crud Operations

--- Server/config/userconfigs.js
+++ Server/config/userconfigs.js
@@ -6,10 +6,10 @@ const port=3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Brookyln@99", 
-    database: "userdata",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 db.connect((err) => {
     if (err) {
