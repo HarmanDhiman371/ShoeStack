d66681a6ea5f08c7d704d12d7429b41e851c5bd3
Harman Dhiman <harmandhimn6776@gmail.com>
2025-02-28 14:30:52 +0530

    created payment route

---
+++ Server/routes/paymentRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { createPaymentIntent,chargePayment, handleWebhook } = require('../controllers/PaymentController');
+const router = express.Router();
+
+router.post('/create-payment-intent', createPaymentIntent);
+router.post('/webhook', express.raw({ type: 'application/json' }), handleWebhook);
+
+
+// Route to handle payment charge after token and clientSecret are received
+router.post('/charge', chargePayment);  // New route for handling charge
+
+
+
+
+module.exports = router;
