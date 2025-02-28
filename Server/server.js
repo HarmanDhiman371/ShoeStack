cc14a5cf83e34c16b251e04eecc0bbe7e59434d7
Harman Dhiman <harmandhimn6776@gmail.com>
2025-02-28 19:31:20 +0530

    stripe working

--- Server/server.js
+++ Server/server.js
@@ -5,13 +5,14 @@ const express = require("express");
 const orderRoutes = require("./routes/orderRoutes");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require('./routes/productRoutes');
-
+const paymentRoutes = require('./routes/paymentRoutes');
 const app = express();
 app.use(express.json());
 
 app.use("/api/orders", orderRoutes);
 app.use("/api", userRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/payments',  paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
