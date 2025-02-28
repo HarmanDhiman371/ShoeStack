6b543afcec0abce964b57cd7b38162aab9a19877
ishpreetkaurr <ishpreetarora01@gmail.com>
2025-02-28 14:41:02 +0530

    product management(crud operations)

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const orderRoutes = require("./routes/orderRoutes");
+const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/orders", orderRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
