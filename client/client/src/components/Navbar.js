a816bc1383917c39e9c80dc0ee5046bc7fedcd9b
Harman Dhiman <harmandhimn6776@gmail.com>
2025-04-09 21:19:28 +0530

    adding navbar

--- client/client/src/components/Navbar.js
+++ client/client/src/components/Navbar.js
@@ -1,62 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import styled from "styled-components";
+import React from 'react';
+import './Navbar.css';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <NavContainer>
-      <div className="logo">
-        <Link to="/">E-Learning3</Link>
+    <nav className="navbar">
+      <div className="navbar-logo">ShoeStack</div>
+      <div className="navbar-right">
+        <ul className="navbar-menu">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#features">Products</a></li>
+          <li><a href="#about">Acessories</a></li>
+          <li><a href="#contact">About</a></li>
+        </ul>
+        <button className="navbar-btn">Get Started</button>
       </div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/cart"><FaShoppingCart /> Cart</Link></li>
-        <li><Link to="/profile"><FaUser /> Profile</Link></li>
-      </ul>
-    </NavContainer>
+    </nav>
   );
-};
+}
 
 export default Navbar;
-
-const NavContainer = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 15px 40px;
-  background: #222;
-  color: #fff;
-
-  .logo a {
-    font-size: 24px;
-    font-weight: bold;
-    color: #fff;
-    text-decoration: none;
-  }
-
-  .nav-links {
-    list-style: none;
-    display: flex;
-    gap: 20px;
-  }
-
-  .nav-links li {
-    display: flex;
-    align-items: center;
-  }
-
-  .nav-links a {
-    color: #fff;
-    text-decoration: none;
-    font-size: 18px;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-  }
-
-  .nav-links a:hover {
-    color: #f8b400;
-  }
-`;
