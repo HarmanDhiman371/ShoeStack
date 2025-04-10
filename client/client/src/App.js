af947910325dcad35e346dc9ee93bda8ee851407
ishpreetkaurr <ishpreetarora01@gmail.com>
2025-04-10 09:50:03 +0530

    changes

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductsPage from './components/ProductsPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// import Home from "./pages/Home";
+// import Products from "./pages/Products";
+// import Cart from "./pages/Cart";
+// import Profile from "./pages/Profile";
+import PaymentForm from "./components/PaymentForm";
+import CartPage from "./components/Cart";
+import ProductPage from "./components/ProductPage";
+import { CartProvider } from './components/CartContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProductsPage />} />
-        {/* You can add more routes here */}
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/products" element={<ProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/payment" element={<PaymentForm />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </CartProvider>
   );
 }
 
