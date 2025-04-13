ed3608380e6c3b56776242c5547a1ef26129de9a
hind0025 <s.hindveer2004@gmail.com>
2025-04-13 18:42:36 +0530

    frontend login

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import Home from "./pages/Home";
-// import Products from "./pages/Products";
-// import Cart from "./pages/Cart";
-// import Profile from "./pages/Profile";
 import PaymentForm from "./components/PaymentForm";
 import CartPage from "./components/Cart";
 import ProductsPage from "./components/ProductsPage";
 import { CartProvider } from './components/CartContext';
 import Body from "./pages/Body";
+import LoginPage from "./pages/loginpage"; 
+
 function App() {
   return (
     <CartProvider>
@@ -18,13 +16,13 @@ function App() {
         <Navbar />
         <main>
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/login" element={<LoginPage />} /> {/* Login route */}
             <Route path="/products" element={<ProductsPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/payment" element={<PaymentForm />} />
           </Routes>
         </main>
-        <Body />
+        <Footer />
       </Router>
     </CartProvider>
   );
