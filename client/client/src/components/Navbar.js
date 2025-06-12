c49019b2b5976d97f18986bad305eab16f94a03f
Harman Dhiman <harmandhiman6776@gmail.com>
2025-06-12 21:03:08 +0530

    making navbar propeerly responsive

--- client/client/src/components/Navbar.js
+++ client/client/src/components/Navbar.js
@@ -1,59 +1,49 @@
 import React from "react";
-//import { Link } from "react-router-dom";
 import { Link, useNavigate } from "react-router-dom";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import styled from "styled-components";
-import { useCart } from "./CartContext"; // Make sure the path is correct
+import { FaShoppingCart, FaUser, FaBars, FaTimes } from "react-icons/fa";
+import { useCart } from "./CartContext";
 import './Navbar.css';
 
 const Navbar = () => {
   const { cartItems } = useCart();
   const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
-  const navigate = useNavigate(); // Initialize navigate function
+  const navigate = useNavigate();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
 
   const handleGetStarted = () => {
-    navigate('/signup'); // Navigate to the login page
-    // Alternatively, use navigate('/signup') to go to the signup page
+    navigate('/signup');
   };
 
-  // For conditional rendering, you would likely want to check if the user is logged in
-  const isLoggedIn = localStorage.getItem('token');  // Check if JWT token exists in localStorage
-  
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">ShoeStack</div>
-      <div className="navbar-right">
+      
+      <div className={`navbar-right ${isMobileMenuOpen ? "active" : ""}`}>
         <ul className="navbar-menu">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/accessories">Accessories</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li><Link to="/" onClick={() => setIsMobileMenuOpen(false)}>Home</Link></li>
+          <li><Link to="/products" onClick={() => setIsMobileMenuOpen(false)}>Products</Link></li>
+          <li><Link to="/accessories" onClick={() => setIsMobileMenuOpen(false)}>Accessories</Link></li>
+          <li><Link to="/about" onClick={() => setIsMobileMenuOpen(false)}>About</Link></li>
 
           <li>
-            <Link to="/cart" className="cart-link">
+            <Link to="/cart" className="cart-link" onClick={() => setIsMobileMenuOpen(false)}>
               <FaShoppingCart />
               {totalItems > 0 && <span className="cart-count">{totalItems}</span>}
             </Link>
           </li>
-
-          {/* Conditionally render Login/Signup or Profile and Logout */}
-          {/* {!isLoggedIn ? (
-            <>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/signup">Signup</Link></li>
-            </>
-          ) : (
-            <>
-              <li><Link to="/profile"><FaUser /> Profile</Link></li>
-              <li><Link to="/logout">Logout</Link></li>
-            </>
-          )} */}
         </ul>
         <button className="navbar-btn" onClick={handleGetStarted}>Get Started</button>
+      </div>
+
+      <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
